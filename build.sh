013a13b71e42c8121186f46f365eaef7833bc653
hienpkam05 <hienpkam1010@gmail.com>
2026-07-06 14:28:14 +0700

    fix build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --noinput
 python manage.py createsuperuser --noinput || true
